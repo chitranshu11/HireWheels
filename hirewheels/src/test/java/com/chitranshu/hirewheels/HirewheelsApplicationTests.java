9e70a3c33b20228e54de9603ddcac3426e15fc23
Chitranshu Gour <chitranshugour67@gmail.com>
2021-03-08 16:08:59 +0530

    Spring Project Initialized

---
+++ hirewheels/src/test/java/com/chitranshu/hirewheels/HirewheelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chitranshu.hirewheels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirewheelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
