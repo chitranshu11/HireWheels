9e6f030a56e94359483cee7ee14138d2324d26fd
Chitranshu Gour <chitranshugour67@gmail.com>
2021-03-10 21:01:38 +0530

    UsersDAO Interface Custom methods added

--- hirewheels/src/main/java/com/chitranshu/hirewheels/dao/UserDAO.java
+++ hirewheels/src/main/java/com/chitranshu/hirewheels/dao/UserDAO.java
@@ -1,9 +1,21 @@
 package com.chitranshu.hirewheels.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.chitranshu.hirewheels.entities.Users;
 
 public interface UserDAO extends JpaRepository<Users, Long>{
+	
+	List<Users> findByFirstName(String firstName);
+	
+	List<Users> findByLastName(String lastName);
+	
+	List<Users> findByFirstNameOrLastName(String firstName, String lastName);
+	
+	List<Users> findByEmail(String email);
+
+	List<Users> findByMobileNo(String mobileNo);
 
 }
